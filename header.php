ffb539b63a4b46d79b1e9b713f34d44acb466142
Christopher Wetherill <chris@chriswetherill.me>
2014-05-27 09:47:03 -0400

    Updates to styling

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset='utf-8' />
+  <meta http-equiv="X-UA-Compatible" content="chrome=1" />
+  <meta name="description" content="Applied Statistics: Introduction to Statistical Analysis" />
+  <meta name="keywords" content="Chris Wetherill, Wetherill, Christopher Wetherill">
+  <meta name="author" content="Chris Wetherill" />
+
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&subset=latin' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" media="screen" href="stylesheets/stylesheet.css">
+  <title>Applied Statistics</title>
+</head>
+
+<body>
