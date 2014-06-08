847daf5b32584a2090d592d3e3430dd684ed1525
Christopher Wetherill <chris@chriswetherill.me>
2014-06-07 23:58:37 -0400

    Made index auto-updating

---
+++ chapters.php
@@ -0,0 +1,109 @@
+<?php
+    $index = array(
+        "part1" => array(
+            array(
+                "title" => "Preface",
+                "description" => "This chapter includes the motivations for writing this book; why you should care about good statistical analysis; how the book is structured; what software and background knowledge are recommended; and some commonly-used statistical terminologies. It's a good place to start if you're new to the game, but perfectly okay to skip if you already have some experience with applied statistics.",
+                "link" => "part1/preface",
+            ),
+            array(
+                "title" => "Introduction to R",
+                "description" => "Throughout this book, R is used to illustrate examples and conduct all analyses. This chapter is meant to serve as a quick primer for those who have not used the software before. Topics include installation, reading data, authoring scripts, and basic data manipulation, graphics generation, and statistical modeling.",
+                "link" => "part1/introToR",
+            ),
+            array(
+                "title" => "Measuring Uncertainty",
+                "description" => "Before working with inferential statistics, you should have a solid understanding of how we quantify uncertainty. Included in this chapter is an overview of measures of spread and central tendency and their important to applied statistics; sampling distributions; methods to visualize and understand data; and a basic discussion of probability.",
+                "link" => "part1/uncertainty",
+            ),
+            array(
+                "title" => "Research Design",
+                "description" => "",
+                "link" => "part1/design",
+            ),
+            array(
+                "title" => "Introduction to Statistical Inference",
+                "description" => "",
+                "link" => "part1/inference",
+            ),
+        ),
+        "part2" => array(
+            array(
+                "title" => "Correlation",
+                "description" => "Correlation is a statistical technique used to illustrate dependence between multiple variables. (I.e., correlation quantifies the degree to which different variables change together.) In this chapter, we give an overview of correlational tests, illustrate when they are appropriate, and discuss their limitations of inference.",
+                "link" => "part2/correlation",
+            ),
+            array(
+                "title" => "Simple Linear Regression",
+                "description" => "",
+                "link" => "part2/regression",
+            ),
+            array(
+                "title" => "Multiple Regression",
+                "description" => "",
+                "link" => "part2/multipleRegression",
+            ),
+            array(
+                "title" => "Related Issues in Regression",
+                "description" => "",
+                "link" => "part2/moreRegression",
+            ),
+        ),
+        "part3" => array(
+            array(
+                "title" => "t-Tools",
+                "description" => "",
+                "link" => "",
+            ),
+            array(
+                "title" => "Alternatives to t-Tools",
+                "description" => "",
+                "link" => "",
+            ),
+            array(
+                "title" => "One-Way ANOVA",
+                "description" => "",
+                "link" => "",
+            ),
+            array(
+                "title" => "Multifactor Studies",
+                "description" => "",
+                "link" => "",
+            ),
+            array(
+                "title" => "Multifactor Studies with Replication",
+                "description" => "",
+                "link" => "",
+            ),
+        ),
+        "part4" => array(
+            array(
+                "title" => "Time-Series Data",
+                "description" => "",
+                "link" => "",
+            ),
+            array(
+                "title" => "Rank-Ordered Data",
+                "description" => "",
+                "link" => "",
+            ),
+            array(
+                "title" => "Counts, Proportions, and Odds",
+                "description" => "",
+                "link" => "",
+            ),
+        ),
+        "part5" => array(
+            array(
+                "title" => "Choosing the Right Analysis: A Cheat Sheet",
+                "description" => "",
+                "link" => "",
+            ),
+            array(
+                "title" => "License",
+                "description" => "",
+                "link" => "license",
+            ),
+        ),
+    );
+?>
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
   $title = "Applied Statistics";
   require_once("header.php");
+  require_once("chapters.php");
+  $count = 1;
 ?>
     <h1 class="title">Applied Statistics</h1>
     <h2 class="title">An Introduction to Statistical Analysis</h2>
@@ -10,72 +12,58 @@
     <h2>Introduction</h2>
     <hr>
     
-    <h3><a href="part1/preface">01 &mdash; Preface</a></h3>
-    <p>This chapter includes the motivations for writing this book; why you should care about good statistical analysis; how the book is structured; what software and background knowledge are recommended; and some commonly-used statistical terminologies. It's a good place to start if you're new to the game, but perfectly okay to skip if you already have some experience with applied statistics.</p>
-    
-    <h3><a href="part1/introToR">02 &mdash; Introduction to R</a></h3>
-    <p>Throughout this book, R is used to illustrate examples and conduct all analyses. This chapter is meant to serve as a quick primer for those who have not used the software before. Topics include installation, reading data, authoring scripts, and basic data manipulation, graphics generation, and statistical modeling.</p>
-    
-    <h3><a href="part1/uncertainty">03 &mdash; Measuring Uncertainty</a></h3>
-    <p>Before working with inferential statistics, you should have a solid understanding of how we quantify uncertainty. Included in this chapter is an overview of measures of spread and central tendency and their important to applied statistics; sampling distributions; methods to visualize and understand data; and a basic discussion of probability.</p>
-    
-    <h3><a href="part1/design">04 &mdash; Research Design</a></h3>
-    <p></p>
-    
-    <h3><a href="part1/inference">05 &mdash; Introduction to Statistical Inference</a></h3>
-    <p></p>
+    <?php
+      for ($i = 0; $i < count($index["part1"]); $i++) {
+        echo "<h3><a href=\"".$index["part1"][$i]["link"]."\">".$count." &mdash; ".$index["part1"][$i]["title"]."</a></h3>";
+        echo "<p>".$index["part1"][$i]["description"]."</p>";
+        $count++;
+      }
+    ?>
     
     <h2>Describing Relationships and Predicting Values</h2>
     <hr>
     
-    <h3><a href="part2/correlation">06 &mdash; Correlation</a></h3>
-    <p>Correlation is a statistical technique used to illustrate dependence between multiple variables. (I.e., correlation quantifies the degree to which different variables change together.) In this chapter, we give an overview of correlational tests, illustrate when they are appropriate, and discuss their limitations of inference.</p>
-    
-    <h3><a href="part2/regression">07 &mdash; Simple Linear Regression</a></h3>
-    <p></p>
-    
-    <h3><a href="part2/multipleRegression">08 &mdash; Multiple Regression</a></h3>
-    <p></p>
-    
-    <h3><a href="part2/moreRegression">09 &mdash; Related Issues in Regression</a></h3>
-    <p></p>
+    <?php
+      for ($i = 0; $i < count($index["part2"]); $i++) {
+        echo "<h3><a href=\"".$index["part2"][$i]["link"]."\">".$count." &mdash; ".$index["part2"][$i]["title"]."</a></h3>";
+        echo "<p>".$index["part2"][$i]["description"]."</p>";
+        $count++;
+      }
+    ?>
     
     <h2>Comparisons Among Multiple Samples</h2>
     <hr>
     
-    <h3>10 &mdash; t-Tools</h3>
-    <p></p>
-    
-    <h3>11 &mdash; Alternatives to t-Tools</h3>
-    <p></p>
-    
-    <h3>12 &mdash; One-Way ANOVA</h3>
-    <p></p>
-    
-    <h3>13 &mdash; Multifactor Studies</h3>
-    <p></p>
-    
-    <h3>14 &mdash; Multifactor Studies with Replication</h3>
-    <p></p>
+    <?php
+      for ($i = 0; $i < count($index["part3"]); $i++) {
+        echo "<h3><a href=\"".$index["part3"][$i]["link"]."\">".$count." &mdash; ".$index["part3"][$i]["title"]."</a></h3>";
+        echo "<p>".$index["part3"][$i]["description"]."</p>";
+        $count++;
+      }
+    ?>
     
     <h2>Inferences from Unusual Data Structures</h2>
     <hr>
     
-    <h3>15 &mdash; Time-Series Data</h3>
-    <p></p>
-    
-    <h3>16 &mdash; Rank-Ordered Data</h3>
-    <p></p>
-    
-    <h3>17 &mdash; Counts, Proportions, and Odds</h3>
-    <p></p>
+    <?php
+      for ($i = 0; $i < count($index["part4"]); $i++) {
+        echo "<h3><a href=\"".$index["part4"][$i]["link"]."\">".$count." &mdash; ".$index["part4"][$i]["title"]."</a></h3>";
+        echo "<p>".$index["part4"][$i]["description"]."</p>";
+        $count++;
+      }
+    ?>
     
     <h2>Supplemental Materials</h2>
     <hr>
     
-    <h3>Choosing the Right Analysis: A Cheat Sheet</h3>
-    
-    <h3><a href="license" target="_blank">License</a></h3>
+    <?php
+      for ($i = 0; $i < count($index["part5"]); $i++) {
+        echo "<h3><a href=\"".$index["part5"][$i]["link"]."\">".$count." &mdash; ".$index["part5"][$i]["title"]."</a></h3>";
+        echo "<p>".$index["part5"][$i]["description"]."</p>";
+        $count++;
+      }
+    ?>
+    <br /><br />
   </div>
 </body>
 </html>
