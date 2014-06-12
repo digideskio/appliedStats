08cce109359e28e6be4156c13b8e07d33c3ca0e1
Christopher Wetherill <chris@chriswetherill.me>
2014-06-12 14:38:50 -0400

    Added least squares animation

---
+++ part2/js/least.squares.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#least_squares').scianimator({
+	    'images': ['assets/least.squares1.png', 'assets/least.squares2.png', 'assets/least.squares3.png', 'assets/least.squares4.png', 'assets/least.squares5.png', 'assets/least.squares6.png', 'assets/least.squares7.png', 'assets/least.squares8.png', 'assets/least.squares9.png', 'assets/least.squares10.png', 'assets/least.squares11.png', 'assets/least.squares12.png', 'assets/least.squares13.png', 'assets/least.squares14.png', 'assets/least.squares15.png', 'assets/least.squares16.png', 'assets/least.squares17.png', 'assets/least.squares18.png', 'assets/least.squares19.png', 'assets/least.squares20.png', 'assets/least.squares21.png', 'assets/least.squares22.png', 'assets/least.squares23.png', 'assets/least.squares24.png', 'assets/least.squares25.png'],
+	    'width': 700,
+	    'delay': 300,
+	    'loopMode': 'sweep'
+	});
+	$('#least_squares').scianimator('play');
+    });
+})(jQuery);
--- part2/regression.php
+++ part2/regression.php
@@ -8,10 +8,18 @@
     <hr>
     
     <h3>Uses of Simple Linear Regression</h3>
-    <p></p>
+    <p>In the last chapter we touched briefly on the idea of using the value of one variable to predict the value of another. Where correlation is used to describe the magnitude of the relatinoship between two variables, a tool called <span class="dt">simple linear regression</span> takes that relationship and uses it to predict the values of Variable B, given some arbitrary value of Variable A. In this context, however, we refer to the \(x\) variable as our <span class="dt">explanatory variable</span> or our <span class="dt">regressor</span> and to the \(y\) variable as the <span class="dt">response variable</span>.</p>
     
     <h3>Least Squares Fitting</h3>
     <p></p>
+    
+    <script src="js/jquery-1.4.4.min.js"></script>
+    <script src="js/jquery.scianimator.min.js"></script>
+    
+    <div id="least_squares" style="display: inline-block;"></div>
+    
+  	<script src="js/least.squares.js"></script>
+
     
     <h3>Coefficient of Determination</h3>
     <p></p>
